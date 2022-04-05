170e6ddf3f90e5991667f31020f4015ced59e507
jrx <jrepnak@hashicorp.com>
2022-04-05 11:12:08 +0200

    initial commit

---
+++ consul.tf
@@ -0,0 +1,17 @@
+provider "hcp" {
+  client_id     = var.hcp_client_id
+  client_secret = var.hcp_client_secret
+}
+
+resource "hcp_hvn" "example_hvn" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "hcp_consul_cluster" "example_hcp" {
+  hvn_id     = hcp_hvn.example_hvn.hvn_id
+  cluster_id = var.cluster_id
+  tier       = "development"
+  #   public_endpoint = true
+}
---
+++ vpc_peering.tf
@@ -0,0 +1,48 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    workspaces = {
+      name = "net-dev"
+    }
+    hostname     = "app.terraform.io"
+    organization = "jrx"
+  }
+}
+
+provider "aws" {
+  region = data.terraform_remote_state.vpc.outputs.aws_region
+}
+
+resource "hcp_aws_network_peering" "peer" {
+  hvn_id          = hcp_hvn.example_hvn.hvn_id
+  peering_id      = var.peering_id
+  peer_vpc_id     = data.terraform_remote_state.vpc.outputs.aws_vpc_id
+  peer_account_id = data.terraform_remote_state.vpc.outputs.aws_owner_id
+  peer_vpc_region = data.terraform_remote_state.vpc.outputs.aws_region
+}
+
+resource "hcp_hvn_route" "peer_route" {
+  hvn_link         = hcp_hvn.example_hvn.self_link
+  hvn_route_id     = var.route_id
+  destination_cidr = data.terraform_remote_state.vpc.outputs.aws_cidr
+  target_link      = hcp_aws_network_peering.peer.self_link
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+  auto_accept               = true
+}
+
+data "aws_route_table" "peer" {
+  vpc_id = data.terraform_remote_state.vpc.outputs.aws_vpc_id
+  filter {
+    name   = "tag:Name"
+    values = ["jrx-dev-vpc-public"]
+  }
+}
+
+resource "aws_route" "peer_route" {
+  route_table_id            = data.aws_route_table.peer.id
+  destination_cidr_block    = hcp_hvn.example_hvn.cidr_block
+  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+}
